f67b5c5fff19052d7e5086cd29d54b1694f29a99
jaime sanchez galvis <jaimesanchezgalvis@gmail.com>
2021-08-16 23:15:35 -0500

    app v1.1

--- src/components/lideres/LiderCard.js
+++ src/components/lideres/LiderCard.js
@@ -2,30 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const LiderCard = ({
-id,
-nombre,
-posicion,
-profesion,
-instagram,
-biografia,
- }) => {
+    id,
+    nombre,
+    posicion,
+    profesion,
+    instagram,
+    biografia,
+}) => {
     return (
-        <div className="card ms-3" style={{maxWidth:340}}>
-            <div className="row no-gutters">
-            <div className="col-md-4">
-                <img src={`./assets/${id}.jpg`} alt={nombre} className="card-img"/>
-            </div>
-            <div className="col-md-8">
-                <div className="card-body">
-                    <h5 className="card-title">
-                    {nombre}
-                    </h5>
-                        <p>-{posicion}<br />-{profesion}<br />-{instagram}</p>
-                <Link to={`./lider/${id}`}>
-                mas...
-                </Link>
+        <div className="card ms-3" style={{ maxWidth: 340 }}>
+            <div className="row g-0">
+                <div className="col-4 col-md-4 col-sm-4">
+                    <img src={`./assets/${id}.jpg`} alt={nombre} className="card-img" />
                 </div>
-            </div>
+                <div className="col-8 col-md-8 col-sm-8">
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {nombre}
+                        </h5>
+                        <p>-{posicion}<br />-{profesion}<br />-{instagram}</p>
+                        <Link to={`./lider/${id}`}>
+                            mas...
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
